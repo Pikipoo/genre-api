c5b4672d82c538c497dc9a48e1a73a68e1db87a4
Pikipoo <anis.bennabi@epitech.eu>
2020-04-02 05:16:18 +0200

    Adding all routes to API

--- genre_api/api.py
+++ genre_api/api.py
@@ -2,11 +2,23 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, abort, marshal_with
 from flask_restful_swagger import swagger
 from peewee import *
+from genre_api.models.meta import sqlite_db as database
+from genre_api.models.genre import Genre
+from genre_api.models.singer import Singer
+from genre_api.models.song import Song
+from genre_api.models.playlist import Playlist
+from genre_api.models.song_to_playlist import SongToPlaylist
+from genre_api.routes.genre import *
+from genre_api.routes.singer import *
+from genre_api.routes.song import *
+from genre_api.routes.playlist import *
 
 
 def create_routes(api):
     api.add_resource(SingerRoute, '/singers')
     api.add_resource(SingerByIDRoute, '/singers/<singer_id>')
+    api.add_resource(SingerSongsRoute, '/singers/<singer_id>/songs')
+    api.add_resource(SingerPlaylistsRoute, '/singers/<singer_id>/playlists')
 
     api.add_resource(GenreRoute, '/genres')
 
